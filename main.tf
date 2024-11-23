2c8dcdaf6fc6ccbe51aeebf47d080d0f31f36653
DouaeChmihi <douaeochmihi@gmail.com>
2024-11-23 18:47:30 +0100

    fix some syntax error in main.tf

--- main.tf
+++ main.tf
@@ -96,6 +96,6 @@ resource "aws_iam_role" "ecs_task_execution" {
 
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_attachment" {
-        role               = aws_iam_role.ecs_task_execution
+        role               = aws_iam_role.ecs_task_execution.name
         policy_arn         = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
